1f24418cd98afa9624d83e984558396d7c6ebb7f
pablopunk <pablovarela182@gmail.com>
2019-05-22 17:56:15 +0200

    oops

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ exports = {
   background: '#1E1F28', // same as black
   white: '#F7FFF7',
   foreground: '#F7FFF7', // same as white
-  red = '#FF6B6B',
-  green = '#2CF6B3',
-  yellow = '#FFE66D',
-  blue = '#69B2E7',
-  magenta = '#CE92CB',
-  cyan = '#4ECDC4'
+  red: '#FF6B6B',
+  green: '#2CF6B3',
+  yellow: '#FFE66D',
+  blue: '#69B2E7',
+  magenta: '#CE92CB',
+  cyan: '#4ECDC4'
 }
