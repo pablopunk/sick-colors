44ac05b086475e936e2041469082d87a4196e0d7
pablopunk <pablovarela182@gmail.com>
2019-06-06 15:55:08 +0200

    New colors

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 module.exports = {
-  black: '#1E1F28',
-  background: '#1E1F28', // same as black
+  black: '#454545',
+  background: '#1E1F28',
   white: '#F7FFF7',
   foreground: '#F7FFF7', // same as white
-  red: '#FF6B6B',
-  green: '#2CF6B3',
-  yellow: '#FFE66D',
-  blue: '#69B2E7',
-  magenta: '#FF6699',
-  cyan: '#4ECDC4',
-  // alternatives
-  ligthBlack: '#282A40',
-  ligthWhite: '#E9E8EB',
-  ligthRed: '#FF866B',
-  ligthGreen: '#5ADAAF',
-  ligthYellow: '#EDD580',
-  ligthBlue: '#4CABD3',
-  ligthMagenta: '#EF7BD5',
-  ligthCyan: '#67D7DE',
+  red: '#F66F6F',
+  green: '#6FF6BE',
+  yellow: '#F6D96F',
+  blue: '#6FB5F6',
+  magenta: '#D36FF6',
+  cyan: '#6FEEF6'
 }
