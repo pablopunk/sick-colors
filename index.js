76c778e6e9d6fc865ef4d6dd94db31318530567a
pablopunk <pablovarela182@gmail.com>
2019-06-10 09:41:18 +0200

    Light palette

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 module.exports = {
   black: '#454545',
-  background: '#1E1F28',
+  background: '#1F1F28',
   white: '#F7FFF7',
-  foreground: '#F7FFF7', // same as white
+  foreground: '#F7FFF7',
   red: '#F66F6F',
   green: '#6FF6BE',
   yellow: '#F6D96F',
   blue: '#6FB5F6',
   magenta: '#D36FF6',
-  cyan: '#6FEEF6'
+  cyan: '#6FEEF6',
+  light: {
+    black: '#454545',
+    background: '#F7FFF7',
+    white: '#F1F1F1',
+    foreground: '#1F1F28',
+    red: '#DF3232',
+    green: '#01B569',
+    yellow: '#C3A437',
+    blue: '#2281D8',
+    magenta: '#B53FDE',
+    cyan: '#18BBC6'
+  }
 }
